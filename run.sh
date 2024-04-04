746dd637d61ab64763e7b086846860c83f0d0119
ayushj05 <joshiayush939@gmail.com>
2024-04-04 19:46:09 +0530

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 sudo apt install libjsoncpp-dev libssl-dev
 g++ src/main.cpp -o bin/main -std=c++17 -ljsoncpp -lssl -lcrypto
-
-if [ $? == 0 ]; then
-    bin/main
-else
-    echo "Compilation failed"
-fi
+bin/main
