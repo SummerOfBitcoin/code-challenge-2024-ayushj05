b95e7afc862a73a2771331ab728409f8eb030ece
ayushj05 <joshiayush939@gmail.com>
2024-04-08 20:56:15 +0530

    Mining only p2wpkh txns

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ void mine() {
     for (auto &txn : transactions) {
         bool flag = false;
         for (auto &inp : txn.second["vin"]) {
-            if (inp["prevout"]["scriptpubkey_type"] != "p2pkh"){
+            if (inp["prevout"]["scriptpubkey_type"] != "v0_p2wpkh"){
                 flag = true;
                 break;
             }
