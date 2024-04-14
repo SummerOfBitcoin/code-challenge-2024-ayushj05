6e6d1d594bd610c055e2f45228b5ac02f28bd189
ayushj05 <joshiayush939@gmail.com>
2024-04-14 23:48:20 +0530

    Mining only p2tr txns

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ void mine() {
     for (auto &txn : transactions) {
         bool flag = false;
         for (auto &inp : txn.second["vin"]) {
-            if (inp["prevout"]["scriptpubkey_type"] != "v0_p2wpkh" && inp["prevout"]["scriptpubkey_type"] != "p2pkh"){
+            if (inp["prevout"]["scriptpubkey_type"] != "v1_p2tr"){
                 flag = true;
                 break;
             }
